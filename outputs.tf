fec229fba2f8f0161d216a64984610112d55b6cb
venu <venubonkuri1234@gmail.com>
2025-07-21 19:31:00 +0530

    checking subnets

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "vpc_id" {
     value = aws_vpc.main.id
 }
+
+output "public_subnet_id" {
+   value = aws_subnet.public[*].id
+}
+#because we are using multiple public subnets, we are outputting a list of IDs
+#and this should be stored in ssm parameter as a comma-separated string
